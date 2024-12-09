cd0945871a6deacb045b03e62493416ba7c3aa1d
Jon Redmond <jon.redmond@klaviyo.com>
2024-12-09 14:02:48 +0000

    Add day6 part 2

--- aoc2024/bin/day_06.ml
+++ aoc2024/bin/day_06.ml
@@ -9,4 +9,7 @@ let () =
   let input = File_utils.read_lines "test_data/day_06.dat" in
   input |> parse_input |> grid_to_array |> fun parsed_input ->
   Printf.printf "Number of unique positions: %i\n"
-    (Day_06.get_number_unique_positions parsed_input)
+    (Day_06.get_number_unique_positions parsed_input);
+  (* Printf.printf "Debugging... %b\n" (Day_06.debug 56 55 parsed_input) *)
+  Printf.printf "Number of looping paths: %i\n"
+    (Day_06.count_number_of_looping_paths parsed_input)
--- aoc2024/lib/day_06.ml
+++ aoc2024/lib/day_06.ml
@@ -51,3 +51,56 @@ let get_number_unique_positions grid =
     starting_position :: get_path grid starting_position starting_direction
   in
   List.length (List.sort_uniq compare path)
+
+let find_loop path last_position =
+  let rec find_loop' path' =
+    match path' with
+    | [] -> []
+    | position :: rest ->
+        if position = last_position then rest else position :: find_loop' rest
+  in
+  find_loop' path
+
+let does_path_loop grid =
+  let starting_position, starting_direction =
+    get_guard_starting_position grid
+  in
+  let rec does_path_loop' visited_nodes path position direction =
+    match check_next_position grid position direction with
+    | None -> false
+    | Some (next_position, next_direction) ->
+        if List.mem position visited_nodes && next_direction <> direction then
+          true
+        else
+          let visited_nodes' =
+            if next_direction = direction then visited_nodes
+            else position :: visited_nodes
+          in
+          does_path_loop' visited_nodes' (next_position :: path) next_position
+            next_direction
+  in
+  does_path_loop' [] [] starting_position starting_direction
+
+let count_number_of_looping_paths grid =
+  let rec count_number_of_looping_paths' x y count =
+    if y >= Array.length grid then count
+    else if x >= Array.length grid.(y) then
+      count_number_of_looping_paths' 0 (y + 1) count
+    else if List.mem grid.(y).(x) [ '#'; '^'; '>'; 'v'; '<' ] then
+      count_number_of_looping_paths' (x + 1) y count
+    else
+      let grid' = Array.copy grid in
+      let original_cell = grid'.(y).(x) in
+      grid'.(y).(x) <- '#';
+      let count' = if does_path_loop grid' then count + 1 else count in
+      grid'.(y).(x) <- original_cell;
+      count_number_of_looping_paths' (x + 1) y count'
+  in
+  count_number_of_looping_paths' 0 0 0
+
+let debug x y grid =
+  print_char grid.(x).(y);
+  flush stdout;
+  let grid' = Array.copy grid in
+  grid'.(y).(x) <- '#';
+  does_path_loop grid'
